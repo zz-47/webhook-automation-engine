6cc73ec152b673831c6ba3086a25f8fbf8e31cf4
zz-47 <786roshaan007@gmail.com>
2025-10-12 23:30:35 +0500

    prototype_1

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # start.sh - starts your Flask bot via start_ngrok.py
 
-python start_ngrok.py
+# Install dependencies
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+
+# Run the bot
+python dark_kitchen_chatbot/start_ngrok.py
